5e6b6e63df43d6ef71f5dd3d712a5e3f26f981e0
msafeiyahya96 <msafeiyahya96@gmail.com>
2022-12-12 23:43:37 +0700

    View to Remove Todo List

--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,5 +1,21 @@
 <?php
+require_once("Helper/Input.php");
+require_once("Controller/RemoveTodoList.php");
 
 function viewRemoveTodoList() {
-    
+    echo "REMOVE TODO" . PHP_EOL;
+
+    $select = input("Number ('x' cancel)");
+
+    if ($select == "x") {
+        echo "Cancel Remove Todo!" . PHP_EOL;
+    } else {
+        $success = removeTodoList($select);
+
+        if ($success) {
+            echo "Success to Remove Todo List Number = $select!" . PHP_EOL;
+        } else {
+            echo "Fail to Remove Todo List Number = $select!" . PHP_EOL;
+        }
+    }
 }
