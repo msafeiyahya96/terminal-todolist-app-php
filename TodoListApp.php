d8da7edc0e2cffa8f46da3157cd665a57540e727
msafeiyahya96 <msafeiyahya96@gmail.com>
2022-12-12 22:20:24 +0700

    create Controllers

--- TodoListApp.php
+++ TodoListApp.php
@@ -1,4 +1,7 @@
 <?php
 require_once("Model/TodoList.php");
+require_once("Controller/ShowTodoList.php");
+require_once("Controller/AddTodoList.php");
+require_once("Controller/RemoveTodoList.php");
 
 echo "Todo List Application" . PHP_EOL;
