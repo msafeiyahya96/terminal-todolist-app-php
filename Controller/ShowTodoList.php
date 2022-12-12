1e6a9dfb8d22cf7ea74dc9b71e149ec98b6c8821
msafeiyahya96 <msafeiyahya96@gmail.com>
2022-12-12 22:34:01 +0700

    Controller Show TodoList

--- Controller/ShowTodoList.php
+++ Controller/ShowTodoList.php
@@ -1,5 +1,11 @@
 <?php
 
 function showTodoList() {
-    
+    global $todoList;
+
+    echo "TODO LIST" . PHP_EOL;
+
+    foreach ($todoList as $number => $todo) {
+        echo "$number. $todo" . PHP_EOL;
+    }
 }
