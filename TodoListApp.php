fd924d99d354438d1a7f3c6109b8a0646f843d03
msafeiyahya96 <msafeiyahya96@gmail.com>
2022-12-12 22:25:23 +0700

    create Views

--- TodoListApp.php
+++ TodoListApp.php
@@ -3,5 +3,8 @@ require_once("Model/TodoList.php");
 require_once("Controller/ShowTodoList.php");
 require_once("Controller/AddTodoList.php");
 require_once("Controller/RemoveTodoList.php");
+require_once("View/ViewShowTodoList.php");
+require_once("View/ViewAddTodoList.php");
+require_once("View/ViewRemoveTodoList.php");
 
 echo "Todo List Application" . PHP_EOL;
