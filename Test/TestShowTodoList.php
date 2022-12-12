b4d1708f7992deaa508a7837144f345160066fd3
msafeiyahya96 <msafeiyahya96@gmail.com>
2022-12-12 22:42:47 +0700

    Test Controller Show Todo List

---
+++ Test/TestShowTodoList.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("Model/TodoList.php");
+require_once("Controller/ShowTodoList.php");
+
+$todoList[1] = "Learn PHP Basic";
+$todoList[2] = "Learn PHP OOP";
+$todoList[3] = "Learn PHP Database";
+
+showTodoList();
