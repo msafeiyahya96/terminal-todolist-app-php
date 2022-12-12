35dad946cbc3e7add7d5bc37a61b0e53c06bb003
msafeiyahya96 <msafeiyahya96@gmail.com>
2022-12-12 23:32:52 +0700

    View to Add Todo List

--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -1,5 +1,16 @@
 <?php
+require_once("Model/TodoList.php");
+require_once("Helper/Input.php");
+require_once("Controller/AddTodoList.php");
 
 function viewAddTodoList() {
-    
+    echo "ADD TODO" . PHP_EOL;
+
+    $todo = input("Todo ('x' exit)");
+
+    if ($todo == "x") {
+        echo "Cancel Add Todo!" . PHP_EOL;
+    } else {
+        addTodoList($todo);
+    }
 }
